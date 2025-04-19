212f68f932fbab6a57b8ac2ff36bae002a46f8ce
Miyakozukakita <terruki0@gmail.com>
2025-04-19 15:44:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, deleteField } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBR7AMsGD3P0lUfjvRHCHjMG3XmK12K4IU",
