f18906e7242e0295e20883e6f9ff230dde7d7750
Miyakozukakita <terruki0@gmail.com>
2025-04-20 22:22:34 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 
-const LINE_ACCESS_TOKEN = "＜あなたのLINEアクセストークン＞";
-const LINE_GROUP_ID = "＜あなたのグループID＞";
+const LINE_ACCESS_TOKEN = "IR54C6+5BAgTTgnZiAwh8kGuCi+3zqzw5jCm4jknpBHz22WPtUp1xXVWnrkDf/NijNoWMwecM3YGT+qvz84Vaau8XdHSD6SXA/JrGIOu7WSQC+xGhAnQcGV5a6rg7lcyzjNZypS0Bn4A9LxLq2uOUgdB04t89/1O/w1cDnyilFU=";
+const LINE_GROUP_ID = "Cf22d3ef700a771c636ff04120cc57fbc";
 
 exports.sendLineMessage = functions.https.onRequest(async (req, res) => {
   const message = req.body.message || "水やり完了の記録がありました！";
